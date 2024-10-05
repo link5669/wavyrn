1b2ca502ed9dedc6e5561117bc636885b0b6efbc
Miles Acquaviva <milesacq@icloud.com>
2024-10-05 10:44:28 -0400

    redirects

--- src/pages/App.jsx
+++ src/pages/App.jsx
@@ -1,19 +1,21 @@
 import { BrowserRouter } from "react-router-dom";
 import { Routes } from "react-router-dom";
 import { Route } from "react-router-dom";
-import Home from "./pages/Home";
+import Home from "./Home";
 import "./App.css";
 import { useState, useEffect } from "react";
-import Services from "./pages/Services";
-import Portfolio from "./pages/Portfolio";
-import AboutUs from "./pages/AboutUs";
-import Contact from "./pages/Contact";
-import WavNavbar from "./components/Navbar";
-import ZoomRedirect from "./pages/ZoomRedirect";
-import DiscordRedirect from "./pages/Discord";
-import Blog from "./pages/blog/blog";
-import Post from "./pages/blog/post";
-import ScrollToTop from "./components/ScrollToTop";
+import Services from "./Services";
+import Portfolio from "./Portfolio";
+import AboutUs from "./AboutUs";
+import Contact from "./Contact";
+import WavNavbar from "../components/Navbar";
+import ZoomRedirect from "./ZoomRedirect";
+import DiscordRedirect from "./Discord";
+import Blog from "./blog/blog";
+import Post from "./blog/post";
+import ScrollToTop from "../components/ScrollToTop";
+import GeneralUseRedirect from "./GeneralUseRedirect";
+import PatreonRedirect from "./PatreonCatalogRedirect";
 
 function App() {
   const [width, setWidth] = useState(window.innerWidth);
@@ -50,6 +52,8 @@ function App() {
             {/* redirects */}
             <Route path="/zoom" element={<ZoomRedirect />} />
             <Route path="/discord" element={<DiscordRedirect />} />
+            <Route path="/GeneralUse" element={<GeneralUseRedirect />} />
+            <Route path="/PatreonCatalogue" element={<PatreonRedirect />} />
 
           </Routes>
         </ScrollToTop>
---
+++ src/pages/GeneralUseRedirect.jsx
@@ -0,0 +1,5 @@
+const GeneralUseRedirect = () => {
+    window.location.href = "https://docs.google.com/document/d/1pqSloLKJl_IHNbWfkuF5BSbRmeBcghry-DG-MFCy0Uc/edit"
+}
+
+export default GeneralUseRedirect
