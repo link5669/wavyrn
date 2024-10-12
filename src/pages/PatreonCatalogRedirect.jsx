ef51622bc94877c3eff7c179bcb7ef6e97917a1d
Miles Acquaviva <milesacq@icloud.com>
2024-10-12 12:28:46 -0400

    Update PatreonCatalogRedirect.jsx

--- src/pages/PatreonCatalogRedirect.jsx
+++ src/pages/PatreonCatalogRedirect.jsx
@@ -1,5 +1,5 @@
 const PatreonRedirect = () => {
-    window.location.href = "https://docs.google.com/document/d/1pqSloLKJl_IHNbWfkuF5BSbRmeBcghry-DG-MFCy0Uc/edit"
+    window.location.href = "https://www.dropbox.com/scl/fo/qckw8mqkg8qtb75b3kr8q/ABWV5ccu-NlJkwMK-3aaIlo?rlkey=kpd5s8e311otjo8svnavbqtys&dl=0"
 }
 
 export default PatreonRedirect
