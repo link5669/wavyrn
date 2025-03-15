381fa2c35a680fbe5742ee8683c8850161930228
link5669 <milesacq@icloud.com>
2025-03-15 22:51:10 +0000

    aaaaa

--- src/components/Overlay/Overlay.jsx
+++ src/components/Overlay/Overlay.jsx
@@ -4,51 +4,56 @@ import { X_svg } from "../../utilities/svgs";
 import "./Overlay.css";
 
 const Overlay = ({ isVisible, onClose, children, profileInfo }) => {
-  useEffect(() => {
-    if (isVisible) {
-      document.body.style.overflow = "hidden";
-    } else {
-      document.body.style.overflow = "unset";
-    }
-    return () => {
-      document.body.style.overflow = "unset";
-    };
-  }, [isVisible]);
+    useEffect(() => {
+        if (isVisible) {
+            document.body.style.overflow = "hidden";
+        } else {
+            document.body.style.overflow = "unset";
+        }
+        return () => {
+            document.body.style.overflow = "unset";
+        };
+    }, [isVisible]);
 
-  return (
-    <div className={`overlay ${isVisible ? "visible" : ""}`}>
-      <div className="overlay-content">
-        <div className="overlay-header">
-          <div className="profile-section">
-            <img
-              src={profileInfo.image}
-              alt={profileInfo.name}
-              className="overlay-profile-pic"
-            />
-            <div className="profile-info">
-              <h3>{profileInfo.name}</h3>
-              <p>{profileInfo.title}</p>
+    return (
+        <div className={`overlay ${isVisible ? "visible" : ""}`}>
+            <div className="overlay-content">
+                <div className="overlay-header">
+                    <div className="profile-section">
+                        <img
+                            src={profileInfo.image}
+                            alt={profileInfo.name}
+                            style={{
+                                objectPosition:
+                                    profileInfo.name == "Zionna Brown" &&
+                                    "0% 25%",
+                            }}
+                            className="overlay-profile-pic"
+                        />
+                        <div className="profile-info">
+                            <h3>{profileInfo.name}</h3>
+                            <p>{profileInfo.title}</p>
+                        </div>
+                    </div>
+                    <button className="close-button" onClick={onClose}>
+                        <svg
+                            xmlns="http://www.w3.org/2000/svg"
+                            width="16"
+                            height="16"
+                            fill="currentColor"
+                            className="bi bi-x"
+                            viewBox="0 0 16 16"
+                        >
+                            <X_svg />
+                        </svg>
+                    </button>
+                </div>
+                <div className="overlay-body">
+                    <div className="scroll-content">{children}</div>
+                </div>
             </div>
-          </div>
-          <button className="close-button" onClick={onClose}>
-            <svg
-              xmlns="http://www.w3.org/2000/svg"
-              width="16"
-              height="16"
-              fill="currentColor"
-              className="bi bi-x"
-              viewBox="0 0 16 16"
-            >
-              <X_svg />
-            </svg>
-          </button>
         </div>
-        <div className="overlay-body">
-          <div className="scroll-content">{children}</div>
-        </div>
-      </div>
-    </div>
-  );
+    );
 };
 
 export default Overlay;
