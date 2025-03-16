85fa71672888a03e3ea22ff12cab0ca05db8e4ba
link5669 <milesacq@icloud.com>
2025-03-16 01:22:02 +0000

    offshored sfx

--- src/utilities/SFX.js
+++ src/utilities/SFX.js
@@ -1,191 +1,191 @@
 const sfx = [
     {
         name: "BLING",
-        link: "https://www.dl.dropboxusercontent.com/scl/fo/tmx340km7moqr280v7if3/h/Website%20Assets/Portfolio/Sound%20Effects/Bling.mp3?rlkey=rgp43tzu84ovmy10j9gni62q5&e=1&dl=0",
+        link: "https://dev.milesacq.com:3006/sound-effects/Bling.mp3",
     },
     {
         name: "BOING",
-        link: "https://www.dl.dropboxusercontent.com/scl/fo/tmx340km7moqr280v7if3/h/Website%20Assets/Portfolio/Sound%20Effects/Boing.mp3?rlkey=rgp43tzu84ovmy10j9gni62q5&e=1&dl=0",
+        link: "https://dev.milesacq.com:3006/sound-effects/Boing.mp3",
     },
     {
         name: "BWEH",
-        link: "https://www.dl.dropboxusercontent.com/scl/fo/tmx340km7moqr280v7if3/h/Website%20Assets/Portfolio/Sound%20Effects/Bweh.mp3?rlkey=rgp43tzu84ovmy10j9gni62q5&e=1&dl=0",
+        link: "https://dev.milesacq.com:3006/sound-effects/Bweh.mp3",
     },
     {
         name: "BWOOSH",
-        link: "https://www.dl.dropboxusercontent.com/scl/fo/tmx340km7moqr280v7if3/h/Website%20Assets/Portfolio/Sound%20Effects/Bwoosh.mp3?rlkey=rgp43tzu84ovmy10j9gni62q5&e=1&dl=0",
+        link: "https://dev.milesacq.com:3006/sound-effects/Bwoosh.mp3",
     },
     {
         name: "CAW",
-        link: "https://www.dl.dropboxusercontent.com/scl/fo/tmx340km7moqr280v7if3/h/Website%20Assets/Portfolio/Sound%20Effects/Caw.mp3?rlkey=rgp43tzu84ovmy10j9gni62q5&e=1&dl=0",
+        link: "https://dev.milesacq.com:3006/sound-effects/Caw.mp3",
     },
     {
         name: "CLACK",
-        link: "https://www.dl.dropboxusercontent.com/scl/fo/tmx340km7moqr280v7if3/h/Website%20Assets/Portfolio/Sound%20Effects/Clack.mp3?rlkey=rgp43tzu84ovmy10j9gni62q5&e=1&dl=0",
+        link: "https://dev.milesacq.com:3006/sound-effects/Clack.mp3",
     },
     {
         name: "CLICK",
-        link: "https://www.dl.dropboxusercontent.com/scl/fo/tmx340km7moqr280v7if3/h/Website%20Assets/Portfolio/Sound%20Effects/Click.mp3?rlkey=rgp43tzu84ovmy10j9gni62q5&e=1&dl=0",
+        link: "https://dev.milesacq.com:3006/sound-effects/Click.mp3",
     },
     {
         name: "CRACKLE",
-        link: "https://www.dl.dropboxusercontent.com/scl/fo/tmx340km7moqr280v7if3/h/Website%20Assets/Portfolio/Sound%20Effects/Crackle.mp3?rlkey=rgp43tzu84ovmy10j9gni62q5&e=1&dl=0",
+        link: "https://dev.milesacq.com:3006/sound-effects/Crackle.mp3",
     },
     {
         name: "CRASH",
-        link: "https://www.dl.dropboxusercontent.com/scl/fo/tmx340km7moqr280v7if3/h/Website%20Assets/Portfolio/Sound%20Effects/Crash.mp3?rlkey=rgp43tzu84ovmy10j9gni62q5&e=1&dl=0",
+        link: "https://dev.milesacq.com:3006/sound-effects/Crash.mp3",
     },
     {
         name: "CREAK",
-        link: "https://www.dl.dropboxusercontent.com/scl/fo/tmx340km7moqr280v7if3/h/Website%20Assets/Portfolio/Sound%20Effects/Creak.mp3?rlkey=rgp43tzu84ovmy10j9gni62q5&e=1&dl=0",
+        link: "https://dev.milesacq.com:3006/sound-effects/Creak.mp3",
     },
     {
         name: "CRUMBLE",
-        link: "https://www.dl.dropboxusercontent.com/scl/fo/tmx340km7moqr280v7if3/h/Website%20Assets/Portfolio/Sound%20Effects/Crumble.mp3?rlkey=rgp43tzu84ovmy10j9gni62q5&e=1&dl=0",
+        link: "https://dev.milesacq.com:3006/sound-effects/Crumble.mp3",
     },
     {
         name: "FLIP",
-        link: "https://www.dl.dropboxusercontent.com/scl/fo/tmx340km7moqr280v7if3/h/Website%20Assets/Portfolio/Sound%20Effects/Flip.mp3?rlkey=rgp43tzu84ovmy10j9gni62q5&e=1&dl=0",
+        link: "https://dev.milesacq.com:3006/sound-effects/Flip.mp3",
     },
     {
         name: "FWOOP",
-        link: "https://www.dl.dropboxusercontent.com/scl/fo/tmx340km7moqr280v7if3/h/Website%20Assets/Portfolio/Sound%20Effects/Fwoop.mp3?rlkey=rgp43tzu84ovmy10j9gni62q5&e=1&dl=0",
+        link: "https://dev.milesacq.com:3006/sound-effects/Fwoop.mp3",
     },
     {
         name: "FWOOSH",
-        link: "https://www.dl.dropboxusercontent.com/scl/fo/tmx340km7moqr280v7if3/h/Website%20Assets/Portfolio/Sound%20Effects/Fwoosh.mp3?rlkey=rgp43tzu84ovmy10j9gni62q5&e=1&dl=0",
+        link: "https://dev.milesacq.com:3006/sound-effects/Fwoosh.mp3",
     },
     {
         name: "FWUMP",
-        link: "https://www.dl.dropboxusercontent.com/scl/fo/tmx340km7moqr280v7if3/h/Website%20Assets/Portfolio/Sound%20Effects/Fwump.mp3?rlkey=rgp43tzu84ovmy10j9gni62q5&e=1&dl=0",
+        link: "https://dev.milesacq.com:3006/sound-effects/Fwump.mp3",
     },
     {
         name: "GLIMMER",
-        link: "https://www.dl.dropboxusercontent.com/scl/fo/tmx340km7moqr280v7if3/h/Website%20Assets/Portfolio/Sound%20Effects/Glimmer.mp3?rlkey=rgp43tzu84ovmy10j9gni62q5&e=1&dl=0",
+        link: "https://dev.milesacq.com:3006/sound-effects/Glimmer.mp3",
     },
     {
         name: "GLUG",
-        link: "https://www.dl.dropboxusercontent.com/scl/fo/tmx340km7moqr280v7if3/h/Website%20Assets/Portfolio/Sound%20Effects/Glug.mp3?rlkey=rgp43tzu84ovmy10j9gni62q5&e=1&dl=0",
+        link: "https://dev.milesacq.com:3006/sound-effects/Glug.mp3",
     },
     {
         name: "HISS",
-        link: "https://www.dl.dropboxusercontent.com/scl/fo/tmx340km7moqr280v7if3/h/Website%20Assets/Portfolio/Sound%20Effects/Hiss.mp3?rlkey=rgp43tzu84ovmy10j9gni62q5&e=1&dl=0",
+        link: "https://dev.milesacq.com:3006/sound-effects/Hiss.mp3",
     },
     {
         name: "JINGLE",
-        link: "https://www.dl.dropboxusercontent.com/scl/fo/tmx340km7moqr280v7if3/h/Website%20Assets/Portfolio/Sound%20Effects/Jingle.mp3?rlkey=rgp43tzu84ovmy10j9gni62q5&e=1&dl=0",
+        link: "https://dev.milesacq.com:3006/sound-effects/Jingle.mp3",
     },
     {
         name: "KLANG",
-        link: "https://www.dl.dropboxusercontent.com/scl/fo/tmx340km7moqr280v7if3/h/Website%20Assets/Portfolio/Sound%20Effects/Klang.mp3?rlkey=rgp43tzu84ovmy10j9gni62q5&e=1&dl=0",
+        link: "https://dev.milesacq.com:3006/sound-effects/Klang.mp3",
     },
     {
         name: "KLING",
-        link: "https://www.dl.dropboxusercontent.com/scl/fo/tmx340km7moqr280v7if3/h/Website%20Assets/Portfolio/Sound%20Effects/Klang.mp3?rlkey=rgp43tzu84ovmy10j9gni62q5&e=1&dl=0",
+        link: "https://dev.milesacq.com:3006/sound-effects/Klang.mp3",
     },
     {
         name: "PEW",
-        link: "https://www.dl.dropboxusercontent.com/scl/fo/tmx340km7moqr280v7if3/h/Website%20Assets/Portfolio/Sound%20Effects/Pew.mp3?rlkey=rgp43tzu84ovmy10j9gni62q5&e=1&dl=0",
+        link: "https://dev.milesacq.com:3006/sound-effects/Pew.mp3",
     },
     {
         name: "PHIZZLE",
-        link: "https://www.dl.dropboxusercontent.com/scl/fo/tmx340km7moqr280v7if3/h/Website%20Assets/Portfolio/Sound%20Effects/Phizzle.mp3?rlkey=rgp43tzu84ovmy10j9gni62q5&e=1&dl=0",
+        link: "https://dev.milesacq.com:3006/sound-effects/Phizzle.mp3",
     },
     {
         name: "PLOP",
-        link: "https://www.dl.dropboxusercontent.com/scl/fo/tmx340km7moqr280v7if3/h/Website%20Assets/Portfolio/Sound%20Effects/Plop.mp3?rlkey=rgp43tzu84ovmy10j9gni62q5&e=1&dl=0",
+        link: "https://dev.milesacq.com:3006/sound-effects/Plop.mp3",
     },
     {
         name: "SCRIBBLE",
-        link: "https://www.dl.dropboxusercontent.com/scl/fo/tmx340km7moqr280v7if3/h/Website%20Assets/Portfolio/Sound%20Effects/Scribble.mp3?rlkey=rgp43tzu84ovmy10j9gni62q5&e=1&dl=0",
+        link: "https://dev.milesacq.com:3006/sound-effects/Scribble.mp3",
     },
     {
         name: "SHATTER",
-        link: "https://www.dl.dropboxusercontent.com/scl/fo/tmx340km7moqr280v7if3/h/Website%20Assets/Portfolio/Sound%20Effects/Shatter.mp3?rlkey=rgp43tzu84ovmy10j9gni62q5&e=1&dl=0",
+        link: "https://dev.milesacq.com:3006/sound-effects/Shatter.mp3",
     },
     {
         name: "SHIMMER",
-        link: "https://www.dl.dropboxusercontent.com/scl/fo/tmx340km7moqr280v7if3/h/Website%20Assets/Portfolio/Sound%20Effects/Shimmer.mp3?rlkey=rgp43tzu84ovmy10j9gni62q5&e=1&dl=0",
+        link: "https://dev.milesacq.com:3006/sound-effects/Shimmer.mp3",
     },
     {
         name: "SHING",
-        link: "https://www.dl.dropboxusercontent.com/scl/fo/tmx340km7moqr280v7if3/h/Website%20Assets/Portfolio/Sound%20Effects/Shing.mp3?rlkey=rgp43tzu84ovmy10j9gni62q5&e=1&dl=0",
+        link: "https://dev.milesacq.com:3006/sound-effects/Shing.mp3",
     },
     {
         name: "SHWOOP",
-        link: "https://www.dl.dropboxusercontent.com/scl/fo/tmx340km7moqr280v7if3/h/Website%20Assets/Portfolio/Sound%20Effects/Shwoop.mp3?rlkey=rgp43tzu84ovmy10j9gni62q5&e=1&dl=0",
+        link: "https://dev.milesacq.com:3006/sound-effects/Shwoop.mp3",
     },
     {
         name: "SIZZLE",
-        link: "https://www.dl.dropboxusercontent.com/scl/fo/tmx340km7moqr280v7if3/h/Website%20Assets/Portfolio/Sound%20Effects/Sizzle.mp3?rlkey=rgp43tzu84ovmy10j9gni62q5&e=1&dl=0",
+        link: "https://dev.milesacq.com:3006/sound-effects/Sizzle.mp3",
     },
     {
         name: "SLOSH",
-        link: "https://www.dl.dropboxusercontent.com/scl/fo/tmx340km7moqr280v7if3/h/Website%20Assets/Portfolio/Sound%20Effects/Slosh.mp3?rlkey=rgp43tzu84ovmy10j9gni62q5&e=1&dl=0",
+        link: "https://dev.milesacq.com:3006/sound-effects/Slosh.mp3",
     },
     {
         name: "SPOLSH",
-        link: "https://www.dl.dropboxusercontent.com/scl/fo/tmx340km7moqr280v7if3/h/Website%20Assets/Portfolio/Sound%20Effects/Splosh.mp3?rlkey=rgp43tzu84ovmy10j9gni62q5&e=1&dl=0",
+        link: "https://dev.milesacq.com:3006/sound-effects/Splosh.mp3",
     },
     {
         name: "SQUELCH",
-        link: "https://www.dl.dropboxusercontent.com/scl/fo/tmx340km7moqr280v7if3/h/Website%20Assets/Portfolio/Sound%20Effects/Squelch.mp3?rlkey=rgp43tzu84ovmy10j9gni62q5&e=1&dl=0",
+        link: "https://dev.milesacq.com:3006/sound-effects/Squelch.mp3",
     },
     {
         name: "SWISH",
-        link: "https://www.dl.dropboxusercontent.com/scl/fo/tmx340km7moqr280v7if3/h/Website%20Assets/Portfolio/Sound%20Effects/Swish.mp3?rlkey=rgp43tzu84ovmy10j9gni62q5&e=1&dl=0",
+        link: "https://dev.milesacq.com:3006/sound-effects/Swish.mp3",
     },
     {
         name: "SWOOSH",
-        link: "https://www.dl.dropboxusercontent.com/scl/fo/tmx340km7moqr280v7if3/h/Website%20Assets/Portfolio/Sound%20Effects/Swoosh.mp3?rlkey=rgp43tzu84ovmy10j9gni62q5&e=1&dl=0",
+        link: "https://dev.milesacq.com:3006/sound-effects/Swoosh.mp3",
     },
     {
         name: "THUNK",
-        link: "https://www.dl.dropboxusercontent.com/scl/fo/tmx340km7moqr280v7if3/h/Website%20Assets/Portfolio/Sound%20Effects/Thunk.mp3?rlkey=rgp43tzu84ovmy10j9gni62q5&e=1&dl=0",
+        link: "https://dev.milesacq.com:3006/sound-effects/Thunk.mp3",
     },
     {
         name: "TWANG",
-        link: "https://www.dl.dropboxusercontent.com/scl/fo/tmx340km7moqr280v7if3/h/Website%20Assets/Portfolio/Sound%20Effects/Twang.mp3?rlkey=rgp43tzu84ovmy10j9gni62q5&e=1&dl=0",
+        link: "https://dev.milesacq.com:3006/sound-effects/Twang.mp3",
     },
     {
         name: "TWINKLE",
-        link: "https://www.dl.dropboxusercontent.com/scl/fo/tmx340km7moqr280v7if3/h/Website%20Assets/Portfolio/Sound%20Effects/Twinkle.mp3?rlkey=rgp43tzu84ovmy10j9gni62q5&e=1&dl=0",
+        link: "https://dev.milesacq.com:3006/sound-effects/Twinkle.mp3",
     },
     {
         name: "WHEEE",
-        link: "https://www.dl.dropboxusercontent.com/scl/fo/tmx340km7moqr280v7if3/h/Website%20Assets/Portfolio/Sound%20Effects/Wheee.mp3?rlkey=rgp43tzu84ovmy10j9gni62q5&e=1&dl=0",
+        link: "https://dev.milesacq.com:3006/sound-effects/Wheee.mp3",
     },
     {
         name: "WHIP",
-        link: "https://www.dl.dropboxusercontent.com/scl/fo/tmx340km7moqr280v7if3/h/Website%20Assets/Portfolio/Sound%20Effects/Whip.mp3?rlkey=rgp43tzu84ovmy10j9gni62q5&e=1&dl=0",
+        link: "https://dev.milesacq.com:3006/sound-effects/Whip.mp3",
     },
     {
         name: "WHIR",
-        link: "https://www.dl.dropboxusercontent.com/scl/fo/tmx340km7moqr280v7if3/h/Website%20Assets/Portfolio/Sound%20Effects/Whir.mp3?rlkey=rgp43tzu84ovmy10j9gni62q5&e=1&dl=0",
+        link: "https://dev.milesacq.com:3006/sound-effects/Whir.mp3",
     },
     {
         name: "WOOF",
-        link: "https://www.dl.dropboxusercontent.com/scl/fo/tmx340km7moqr280v7if3/h/Website%20Assets/Portfolio/Sound%20Effects/Woof.mp3?rlkey=rgp43tzu84ovmy10j9gni62q5&e=1&dl=0",
+        link: "https://dev.milesacq.com:3006/sound-effects/Woof.mp3",
     },
     {
         name: "WHOOP",
-        link: "https://www.dl.dropboxusercontent.com/scl/fo/tmx340km7moqr280v7if3/h/Website%20Assets/Portfolio/Sound%20Effects/Woop.mp3?rlkey=rgp43tzu84ovmy10j9gni62q5&e=1&dl=0",
+        link: "https://dev.milesacq.com:3006/sound-effects/Woop.mp3",
     },
     {
         name: "ZAP",
-        link: "https://www.dl.dropboxusercontent.com/scl/fo/tmx340km7moqr280v7if3/h/Website%20Assets/Portfolio/Sound%20Effects/Zap.mp3?rlkey=rgp43tzu84ovmy10j9gni62q5&e=1&dl=0",
+        link: "https://dev.milesacq.com:3006/sound-effects/Zap.mp3",
     },
     {
         name: "ZING",
-        link: "https://www.dl.dropboxusercontent.com/scl/fo/tmx340km7moqr280v7if3/h/Website%20Assets/Portfolio/Sound%20Effects/Zing.mp3?rlkey=rgp43tzu84ovmy10j9gni62q5&e=1&dl=0",
+        link: "https://dev.milesacq.com:3006/sound-effects/Zing.mp3",
     },
     {
         name: "ZWOOP",
-        link: "https://www.dl.dropboxusercontent.com/scl/fo/tmx340km7moqr280v7if3/h/Website%20Assets/Portfolio/Sound%20Effects/Zwoop.mp3?rlkey=rgp43tzu84ovmy10j9gni62q5&e=1&dl=0",
+        link: "https://dev.milesacq.com:3006/sound-effects/Zwoop.mp3",
     },
     {
         name: "ZWOOSH",
-        link: "https://www.dl.dropboxusercontent.com/scl/fo/tmx340km7moqr280v7if3/h/Website%20Assets/Portfolio/Sound%20Effects/Zwoosh.mp3?rlkey=rgp43tzu84ovmy10j9gni62q5&e=1&dl=0",
+        link: "https://dev.milesacq.com:3006/sound-effects/Zwoosh.mp3",
     },
 ];
 
