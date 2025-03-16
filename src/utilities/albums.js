136cde05704c404a941cf2e6e533ec179ba10227
link5669 <milesacq@icloud.com>
2025-03-16 14:10:43 +0000

    offshoring

--- src/utilities/albums.js
+++ src/utilities/albums.js
@@ -3,105 +3,94 @@ const albumData = [
         id: 2,
         title: "Coffee and Oil",
         coverUrl:
-            "https://www.dl.dropboxusercontent.com/scl/fo/tmx340km7moqr280v7if3/h/Website%20Assets/Portfolio/Music/_Album%20Covers/ATAS-TEHA%20Album%20Cover.png?rlkey=rgp43tzu84ovmy10j9gni62q5&e=1&dl=0",
+            "https://link5669.github.io/wavyrn-media/music/art/ATAS-TEHA.webp",
         track: "https://link5669.github.io/wavyrn-media/music/10.mp3",
     },
     {
         id: 3,
         title: "Main Theme",
-        coverUrl:
-            "https://www.dl.dropboxusercontent.com/scl/fo/tmx340km7moqr280v7if3/h/Website%20Assets/Portfolio/Music/_Album%20Covers/ROTS%20Album%20Cover.png?rlkey=rgp43tzu84ovmy10j9gni62q5&e=1&dl=0",
+        coverUrl: "https://link5669.github.io/wavyrn-media/music/art/ROTS.webp",
         track: "https://link5669.github.io/wavyrn-media/music/11.mp3",
     },
     {
         id: 4,
         title: "Don't Play With Knives!!",
-        coverUrl:
-            "https://www.dl.dropboxusercontent.com/scl/fo/tmx340km7moqr280v7if3/h/Website%20Assets/Portfolio/Music/_Album%20Covers/SRVR%20Album%20Cover.png?rlkey=rgp43tzu84ovmy10j9gni62q5&e=1&dl=0",
+        coverUrl: "https://link5669.github.io/wavyrn-media/music/art/SRVR.webp",
         track: "https://link5669.github.io/wavyrn-media/music/12.mp3",
     },
     {
         id: 5,
         title: "Busy Hour",
-        coverUrl:
-            "https://www.dl.dropboxusercontent.com/scl/fo/tmx340km7moqr280v7if3/h/Website%20Assets/Portfolio/Music/_Album%20Covers/BTND%20Album%20Cover.png?rlkey=rgp43tzu84ovmy10j9gni62q5&e=1&dl=0",
+        coverUrl: "https://link5669.github.io/wavyrn-media/music/art/BTND.webp",
         track: "https://link5669.github.io/wavyrn-media/music/13.mp3",
     },
     {
         id: 6,
         title: "Ordinary Student Life",
-        coverUrl:
-            "https://www.dl.dropboxusercontent.com/scl/fo/tmx340km7moqr280v7if3/h/Website%20Assets/Portfolio/Music/_Album%20Covers/ARNC%20Album%20Cover.png?rlkey=rgp43tzu84ovmy10j9gni62q5&e=1&dl=0",
+        coverUrl: "https://link5669.github.io/wavyrn-media/music/art/ARNC.webp",
         track: "https://link5669.github.io/wavyrn-media/music/14.mp3",
     },
     {
         id: 7,
         title: "Words of the High One",
         coverUrl:
-            "https://www.dl.dropboxusercontent.com/scl/fo/tmx340km7moqr280v7if3/h/Website%20Assets/Portfolio/Music/_Album%20Covers/DND-NRS%20Album%20Cover.png?rlkey=rgp43tzu84ovmy10j9gni62q5&e=1&dl=0",
+            "https://link5669.github.io/wavyrn-media/music/art/DND-NRS.webp",
         track: "https://link5669.github.io/wavyrn-media/music/15.mp3",
     },
     {
         id: 8,
         title: "A Shining Beacon",
-        coverUrl:
-            "https://www.dl.dropboxusercontent.com/scl/fo/tmx340km7moqr280v7if3/h/Website%20Assets/Portfolio/Music/_Album%20Covers/GLAD%20Album%20Cover.png?rlkey=rgp43tzu84ovmy10j9gni62q5&e=1&dl=0",
+        coverUrl: "https://link5669.github.io/wavyrn-media/music/art/GLAD.webp",
         track: "https://link5669.github.io/wavyrn-media/music/16.mp3",
     },
     {
         id: 9,
         title: "Apocalypse Country",
-        coverUrl:
-            "https://www.dl.dropboxusercontent.com/scl/fo/tmx340km7moqr280v7if3/h/Website%20Assets/Portfolio/Project%20Gallery/43%20Beach%20Trip!%20-%20Short%20Film.png?rlkey=rgp43tzu84ovmy10j9gni62q5&e=1&dl=0",
+        coverUrl: "https://link5669.github.io/wavyrn-media/music/art/BTRP.webp",
         track: "https://link5669.github.io/wavyrn-media/music/17.mp3",
     },
     {
         id: 10,
         title: "Alien Dungeon Crawler",
         coverUrl:
-            "https://www.dl.dropboxusercontent.com/scl/fo/tmx340km7moqr280v7if3/h/Website%20Assets/Portfolio/Music/_Album%20Covers/COSMIC%20Album%20Cover.png?rlkey=rgp43tzu84ovmy10j9gni62q5&e=1&dl=0",
+            "https://link5669.github.io/wavyrn-media/music/art/COSMIC.webp",
         track: "https://link5669.github.io/wavyrn-media/music/3.mp3",
     },
     {
         id: 11,
         title: "Like It's A Dream",
-        coverUrl:
-            "https://www.dl.dropboxusercontent.com/scl/fo/tmx340km7moqr280v7if3/h/Website%20Assets/Portfolio/Music/_Album%20Covers/BWBL%20Album%20Cover.png?rlkey=rgp43tzu84ovmy10j9gni62q5&e=1&dl=0",
+        coverUrl: "https://link5669.github.io/wavyrn-media/music/art/BWBL.webp",
         track: "https://link5669.github.io/wavyrn-media/music/4.mp3",
     },
     {
         id: 12,
         title: "Forest Escape",
-        coverUrl:
-            "https://www.dl.dropboxusercontent.com/scl/fo/tmx340km7moqr280v7if3/h/Website%20Assets/Portfolio/Music/_Album%20Covers/FUGG%20Album%20Cover.png?rlkey=rgp43tzu84ovmy10j9gni62q5&e=1&dl=0",
+        coverUrl: "https://link5669.github.io/wavyrn-media/music/art/FUGG.webp",
         track: "https://link5669.github.io/wavyrn-media/music/5.mp3",
     },
     {
         id: 13,
         title: "Be A Hero",
-        coverUrl:
-            "https://www.dl.dropboxusercontent.com/scl/fo/tmx340km7moqr280v7if3/h/Website%20Assets/Portfolio/Music/_Album%20Covers/GLAD%20Album%20Cover.png?rlkey=rgp43tzu84ovmy10j9gni62q5&e=1&dl=0",
+        coverUrl: "https://link5669.github.io/wavyrn-media/music/art/GLAD.webp",
         track: "https://link5669.github.io/wavyrn-media/music/6.mp3",
     },
     {
         id: 14,
         title: "Grímnismál",
         coverUrl:
-            "https://www.dl.dropboxusercontent.com/scl/fo/tmx340km7moqr280v7if3/h/Website%20Assets/Portfolio/Music/_Album%20Covers/DND-NRS%20Album%20Cover.png?rlkey=rgp43tzu84ovmy10j9gni62q5&e=1&dl=0",
+            "https://link5669.github.io/wavyrn-media/music/art/DND-NRS.webp",
         track: "https://link5669.github.io/wavyrn-media/music/7.mp3",
     },
     {
         id: 15,
         title: "Afterlife Redux",
-        coverUrl:
-            "https://www.dl.dropboxusercontent.com/scl/fo/tmx340km7moqr280v7if3/h/Website%20Assets/Portfolio/Music/_Album%20Covers/AFTL%20Album%20Cover.png?rlkey=rgp43tzu84ovmy10j9gni62q5&e=1&dl=0",
+        coverUrl: "https://link5669.github.io/wavyrn-media/music/art/AFTL.webp",
         track: "https://link5669.github.io/wavyrn-media/music/8.mp3",
     },
     {
         id: 1,
         title: "Fast n' Easy",
-        coverUrl:
-            "https://www.dl.dropboxusercontent.com/scl/fo/tmx340km7moqr280v7if3/h/Website%20Assets/Portfolio/Project%20Gallery/6%20Beat%20Tender%20-%20Rhythm%20Game.png?rlkey=rgp43tzu84ovmy10j9gni62q5&e=1&dl=0",
+        coverUrl: "https://link5669.github.io/wavyrn-media/music/art/BTND.webp",
         track: "https://link5669.github.io/wavyrn-media/music/9.mp3",
     },
 ];
